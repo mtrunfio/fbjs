5e8c18c0cc00981bfd080b1406d5a7beecdc6d42
Mauricio Trunfio <mauricio@renovasolutions.com.br>
2018-10-09 23:16:23 -0300

    Update setImmediate.js

--- packages/fbjs/src/__forks__/setImmediate.js
+++ packages/fbjs/src/__forks__/setImmediate.js
@@ -11,5 +11,5 @@
 
 // setimmediate adds setImmediate to the global. We want to make sure we export
 // the actual function.
-require('setimmediate')
+require('../../setimmediate')
 module.exports = global.setImmediate;
